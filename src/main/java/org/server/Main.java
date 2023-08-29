bdc4c2979e0ee2e55f5394f38895548d46f66746
hyunchuljung <guscjf0903@naver.com>
2023-08-29 14:51:06 +0900

    ChatRoom branchtest

--- src/main/java/org/server/Main.java
+++ src/main/java/org/server/Main.java
@@ -2,6 +2,6 @@ package org.server;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("ㅈㅁㅇㅁㅇqweqwe");
+        System.out.println("branch test");
     }
 }
